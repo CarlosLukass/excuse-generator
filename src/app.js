76398c08716ca4d7babb815479970471f9bbe562
CarlosLukass <carlos.lukassf@gmail.com>
2022-06-02 01:46:11 +0000

    excusegenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function createExcuse(who, what, when) {
+  const $html = document.getElementById("excuse");
+
+  let randomExcusePicked = (max, min) => {
+    return Math.floor(Math.random() * max) - min;
+  };
+
+  let whoPicker = randomExcusePicked(who.length, 0);
+  let whatPicker = randomExcusePicked(what.length, 0);
+  let whenPicker = randomExcusePicked(when.length, 0);
+
+  let excuse = ($html.innerHTML = `${who[whoPicker]} ${what[whatPicker]} my homework ${when[whenPicker]}`);
+
+  return excuse;
+}
+
+createExcuse(who, what, when);
